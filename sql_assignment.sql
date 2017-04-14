62580e3e238c295cba6b17cead1021cccf554a64
Brian Du <brian.bh.du@gmail.com>
2017-04-14 18:43:58 -0400

    SQL Assignment question 1 complete

--- sql_assignment.sql
+++ sql_assignment.sql
@@ -6,6 +6,19 @@
 
 -- 1) Find the albums recorded by the artist Queen.
 
+SELECT * FROM artists WHERE name='Queen';
+ id | name  |         created_at         |         updated_at
+----+-------+----------------------------+----------------------------
+ 51 | Queen | 2006-01-08 22:57:01.166112 | 2014-01-29 22:10:22.166366
+(1 row)
+
+SELECT * FROM albums WHERE artist_id=51;
+ id  | artist_id |       title       |         created_at         |         updated_at
+-----+-----------+-------------------+----------------------------+----------------------------
+  36 |        51 | Greatest Hits II  | 2008-01-30 14:00:12.455894 | 2014-01-29 22:14:02.456155
+ 185 |        51 | Greatest Hits I   | 2007-12-29 12:02:13.672295 | 2014-01-29 22:14:02.672569
+ 186 |        51 | News Of The World | 2010-06-23 20:50:06.971442 | 2014-01-29 22:14:02.971684
+(3 rows)
 
 
 -- 2) [Count](http://www.w3schools.com/sql/sql_func_count.asp) how many tracks belong to the media type "Protected MPEG-4 video file".
