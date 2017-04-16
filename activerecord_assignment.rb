dcb30b5db6c2caf4fcd83ace8e8dc1a99c418685
Brian Du <brian.bh.du@gmail.com>
2017-04-16 15:01:04 -0400

    ActiveRecord Query Interface assignment question 3a complete (updated)

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -29,10 +29,22 @@
 
 # 3a) Find the highest price of any track that has the media type "MPEG audio file".
 
+# MediaType.where(name: 'MPEG audio file')
+#   MediaType Load (0.5ms)  SELECT "media_types".* FROM "media
+# _types" WHERE "media_types"."name" = $1  [["name", "MPEG aud
+# io file"]]
+# => #<ActiveRecord::Relation [#<MediaType id: 1, name: "MPEG
+# audio file", created_at: "2006-11-24 08:21:33", updated_at:
+#
 
 
 # 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
 
+# Track.where(media_type_id: 1).maximum(:unit
+# _price)
+#    (3.0ms)  SELECT MAX("tracks"."unit_price") FROM "tracks"
+# WHERE "tracks"."media_type_id" = $1  [["media_type_id", 1]]
+# => #<BigDecimal:7fcb49249110,'0.99E0',9(18)>
 
 
 # 4) Find the 2 oldest artists.
